79cfe7d9ca06796f42032e72ee30761fa3495aaf
ruan.linhares <ruanvitor0941@gmail.com>
2024-09-16 10:48:38 -0300

    3536inproduction

--- src/Lista03/Exercicio33.java
+++ src/Lista03/Exercicio33.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Exercicio33 {
     public static void main(String[] args) {
+
         Scanner read = new Scanner (System.in);
     }
 }
---
+++ src/Lista03/Exercicio35.java
@@ -0,0 +1,14 @@
+package Lista03;
+
+public class Exercicio35 {
+    public static void main(String[] args) {
+
+        for(int i = 1; i <= 100; i++){
+            if(i % 10 == 0){
+                System.out.println(i + " Multiplo de 10");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}

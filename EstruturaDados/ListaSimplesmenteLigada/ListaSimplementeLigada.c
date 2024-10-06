bf61a5c8577cd51340e98c91a0edb187f99bdc3b
Ruan Linhares <ruanvitor0941@gmail.com>
2024-10-06 18:57:33 -0300

    Att lista simplesmente ligada

--- EstruturaDados/ListaSimplesmenteLigada/ListaSimplementeLigada.c
+++ EstruturaDados/ListaSimplesmenteLigada/ListaSimplementeLigada.c
@@ -1,46 +1,185 @@
+//Bibliotecas
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+#include <string.h>
 
+//Definições
 #define TAM 5
 
-typedef struct Motocicleta{
-    int id;
-    char marca[10];
-    char modelo[10];
-    char cor;
-    int cilindrada;
-    char partida[1];
-    char alimentaÃ§Ã£o[1];
-    float capacidade_carga;
-    float preco;
-} moto;
+//Estruturas
+typedef struct
+{
+	int cod_moto;
+	char marca[20];
+	char modelo[20];
+	char cor;
+	int cilindrada;
+	char partida;
+	char alimentacao;
+	float capacidade_carga;
+	float preco;
+} Motocicleta;
 
-void criarMoto();
+typedef struct
+{
+	int id;
+	Motocicleta *elementos;
+} Lista;
 
-int main (){
-    int opc;
-    printf("MOTOROAD\n");
-    printf("1 - Criar regitro da moto\n2 - Atualizar registro da moto\n3 - Excluir registro da moto");
-    printf("Escolha uma opÃ§Ã£o:");
-    scanf("%d", &opc);
-    
-    switch(opc){
-        case 1:
-            criarMoto();
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        default:
-            printf("OpcÃ£o invÃ¡lida!! OpÃ§Ã£o escolhida nÃ£o consta no sistema...");
-            break;
-    }
-    return 0;
+//Declarando funções
+Lista *criarLista();
+void imprimirElementos(Lista *);
+int inserirElemento(Lista*, Motocicleta);
+
+//Função main
+int main ()
+{
+	setlocale(LC_ALL, "Portuguese");
+	
+	int opc;
+	char continuar = ' ';
+	Motocicleta novaMoto;
+
+	Lista *lista = NULL;
+
+	do
+	{
+		printf("=========MotorRoad========");
+		printf("\n1 - Criar Lista");
+		printf("\n2 - Inserir moto");
+		printf("\n3 - Imprimir Lista de motos");
+		
+		printf("\n");
+		printf("\nEscolha uma opção: ");
+		scanf("%d", &opc);
+		getchar();
+		
+		switch(opc)
+		{
+		case 1:
+			
+			lista = criarLista();
+			break;
+		case 2:
+			
+			printf("-------CADASTRAR NOVA MOTO-------");
+			printf("\nInsira o código: ");
+			scanf("%d", &novaMoto.cod_moto);
+			getchar();
+			printf("\nInsira a marca: ");
+			scanf("%s", &novaMoto.marca);
+			printf("\nInsira o modelo: ");
+			scanf("%s", &novaMoto.modelo);
+			printf("\nInsira o cor: ");
+			scanf("%s", &novaMoto.cor);
+			printf("\nInsira o cilindrada: ");
+			scanf("%d", &novaMoto.cilindrada);
+			printf("\nInsira o partida: ");
+			scanf(" %c", &novaMoto.partida);
+			printf("\nInsira o alimentação: ");
+			scanf(" %c", &novaMoto.alimentacao);
+			printf("\nInsira o carga: ");
+			scanf("%f", &novaMoto.capacidade_carga);
+			printf("\nInsira o preço: ");
+			scanf("%f", &novaMoto.preco);
+
+			inserirElemento(lista, novaMoto);
+
+			break;
+		case 3 :
+			
+			imprimirElementos(lista);
+			break;
+		
+		default:
+			printf("Opção inválida");
+			break;
+		}
+		
+		printf("Deseja continuar?\n");
+		scanf("%c", &continuar);
+		
+	}while(continuar != 's' || continuar != 'S' );
+	return 0;
 }
 
-void criarMoto(){
-    
+//Funções
+
+Lista *criarLista()
+{
+	Lista *newList = (Lista*)malloc(sizeof(Lista));
+
+	if(newList == NULL)
+	{
+		printf("Erro na alocação de memória");
+		return NULL;
+	}
+	newList->id = 0;
+	newList->elementos = (Motocicleta *)malloc(TAM * sizeof(Motocicleta));
+
+	if(newList->elementos == NULL)
+	{
+		printf("\nNão tem espaço");
+		free(newList);
+		return NULL;
+	}
+
+	return newList;
 }
+
+void imprimirElementos(Lista *lista)
+{
+	int i;
+
+	if(lista == NULL)
+	{
+		printf("\nA lista não foi criada");
+	}
+	if(lista->id == 0)
+	{
+		printf("\nLista vazia");
+	}
+
+	for(i = 0; i < lista->id; ++i)
+	{
+		printf("MOTO %d",i);
+		printf("Código: %d\n", lista->elementos[i].cod_moto);
+		printf("Marca: %s\n", lista->elementos[i].marca);
+		printf("Modelo: %s\n", lista->elementos[i].modelo);
+		printf("Cor: %c\n", lista->elementos[i].cor);
+		printf("Cilindrada: %d\n", lista->elementos[i].cilindrada);
+		printf("Partida: %c\n", lista->elementos[i].partida);
+		printf("Alimentação: %c\n", lista->elementos[i].alimentacao);
+		printf("Carga: %f kg\n", lista->elementos[i].capacidade_carga);
+		printf("Preço: R$ %f\n", lista->elementos[i].preco);
+	}
+
+}
+
+
+int inserirElemento(Lista *lista, Motocicleta novaMoto)
+{
+
+	if(lista == NULL)
+	{
+		printf("\nA lista nao foi criada\n");
+		return 0;
+	}
+
+	if(lista->id < TAM)
+	{
+		lista->elementos[lista->id] = novaMoto;
+		++(lista->id);
+	}
+	else
+	{
+		printf("\nEspaco esgotado\n");
+		return 0;
+	}
+
+	return 1;
+}
+
 
 

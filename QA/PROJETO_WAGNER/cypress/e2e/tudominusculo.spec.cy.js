3deca3b195d57035f1f847f93a448481a26ab38b
RUAN VITOR LINHARES FELIX DA SILVA <ruan.linhares@a.ficr.edu.br>
2025-05-20 18:30:07 -0300

    CypressAUTOsistemtest

--- QA/PROJETO_WAGNER/cypress/e2e/tudominusculo.spec.cy.js
+++ QA/PROJETO_WAGNER/cypress/e2e/tudominusculo.spec.cy.js
@@ -13,21 +13,6 @@ describe('template spec', () => {
     "abc"
   ];
 
-  const emailValido = (email) => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  };
-
-  const senhaForte = (senha) => {
-    if (senha.length <= 6 || senha.length >= 3) return false;
-    if (/\s/.test(senha)) return false;
-    if (!/[a-z]/.test(senha)) return false;
-    if (!/[A-Z]/.test(senha)) return false;
-    if (!/[0-9]/.test(senha)) return false;
-    if (!/[^a-zA-Z0-9\s]/.test(senha)) return false;
-    return true;
-  };
-
   it('Testa diferentes combinações de email e senha', () => {
     
     cy.visit('http://127.0.0.1:5500/projetoteste/login.html');
@@ -41,20 +26,21 @@ describe('template spec', () => {
         cy.get('#password').clear().type(senha);
         cy.get('.btn').click();
 
+        cy.wait(1000);
 
-        if (emailValido(email) && senhaForte(senha)) {
-          cy.wait(1000);
-          cy.url().should('include', 'sucesso.html').then(() => {
-            cy.log(`Login bem-sucedido com email: ${email} e senha: ${senha}`);
-            cy.visit('http://127.0.0.1:5500/projetoteste/login.html');
-            cy.get('#email').clear().type(email);
-             // Prepara para a próxima iteração
-          });
-        }else {
-            cy.log(`Tentativa mal-sucedida com email: ${email} e senha: ${senha}`);
-            cy.visit('http://127.0.0.1:5500/projetoteste/login.html');
-            cy.get('#email').clear().type(email);
-            // Aqui você pode adicionar mais verificações se elementos de erro aparecem na página de login
+        const emailSenhaValido = (email === "teste@email.com" && senha === "1@Bc");
+
+        if (emailSenhaValido) {
+          
+          cy.url().should('include', 'sucesso.html');
+          
+          cy.visit('http://127.0.0.1:5500/projetoteste/login.html');
+
+          cy.get('#email').clear().type(email);
+          
+        } else {
+      
+          cy.url().should('include', 'login.html');
         }
       };
     };
